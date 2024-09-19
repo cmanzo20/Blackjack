0bc30d3cebc4909e492a416d1b3646abe74803df
Carlos <cmanzo20@stu.jjc.edu>
2024-09-19 12:33:03 -0500

    Coded function to build deck of cards array in JavaScript

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,25 @@
 //This file will be for potential functions for black jack logic
 
-let totalCash = 1000;   //starting cash value
+let totalCash = 1000;   //starting cash value $1000
+let playerSum = 0;  //player card total
+let dealerSum = 0;  //dealer card total
+let deck = buildDeck();  //deck of cards
+
+//when window loads...
+window.onload = function(){
+    
+}
+
+function buildDeck() {  //returns deck of cards
+    let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+    let suit = ["C", "D", "H", "S"];    //club, diamond, heart, spade
+    let deck = [];  //empty deck array
+
+    for(let i = 0; i < suit.length; i++){
+        for(let j = 0; j < values.length; j++){
+            deck.push(values[j] + "-" + suit[i]);   //appends "SUIT-VALUE"
+        }
+    }
+
+    return deck;    //returns fully built deck of cards
+}
