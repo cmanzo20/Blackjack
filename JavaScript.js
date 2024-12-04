da793e9854475ff5fae5f746b78ab7c132b2f355
Carlos <cmanzo20@stu.jjc.edu>
2024-12-03 18:29:03 -0600

    added numerous JS functions including: displayCard, subtractBet, and updated gamble function to deal cards to dealer and player

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,3 @@
-//This file will be for potential functions for black jack logic
-
 let totalCash = 1000;   //starting cash value $1000
 let playerSum = 0;  //player card total
 let dealerSum = 0;  //dealer card total
@@ -34,7 +32,7 @@ function shuffle(deck){
     }
 }
 
-function getCardValue(card){
+function getCardValue(card){    //returns cards numerical value
     let value_PNG = card.split("-");
     if(isNaN(value_PNG[0])){  //face card
         if(value_PNG[0]== 'A')
@@ -45,11 +43,53 @@ function getCardValue(card){
     return(parseInt(value_PNG[0]));
 }
 
-function updateMaxBet(){
+function updateMaxBet(){    //updates max bet value to remaining balance
     var maxBet = document.getElementById('betAmount');
     maxBet.setAttribute("max", totalCash);  //max bet amount is what balance remains
 }
 
+function subtractBet(){ //deduct bet from total cash
+    betAmount = document.getElementById("betAmount").value;
+    totalCash -= betAmount;
+}
+
+function displayCard(card, displayLocation){    //display card on webpage
+    let CardDisplayLocation = document.getElementById(displayLocation);
+    let img = document.createElement('img');
+    img.src = '/Cards/'+ card+'.png';
+    console.log(card);
+    console.log(img.src);
+    displayLocation.append(img);
+}
+
+
 function Gamble(event){
-    event.preventDefault();
+    event.preventDefault(); //prevent page reload
+    subtractBet();  
+    dealerSum += getCardValue(deck[deck.length-1]); //deal 1st card to dealer
+    displayCard(deck[deck.length-1], DealerCards);
+    deck.pop();
+    playerSum+= getCardValue(deck[deck.length-1]);  //deal 1st card to player
+    displayCard(deck[deck.length-1], PlayerCards);
+    deck.pop();
+    dealerSum += getCardValue(deck[deck.length-1]); //deal 2nd card to dealer
+    displayCard(deck[deck.length-1], DealerCards);
+    deck.pop();
+    playerSum+= getCardValue(deck[deck.length-1]);  //deal 2nd card to player
+    displayCard(deck[deck.length-1], PlayerCards);
+    deck.pop();
+    while(playerSum <21){
+        if (playerSum == 21){
+            alert("Blackjack!");
+            break;
+        }
+        else if(playerSum > 21){
+            alert("BUST");
+            break;
+        }
+        else{
+            alert("hit or stand?");
+            break;
+        }
+    }
 }
